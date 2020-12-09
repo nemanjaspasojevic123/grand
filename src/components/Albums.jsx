492bacc9257c14a5f0dfea1f12524a0ee0046ea9
Nemanja Spasojevic <nemanja.spasojevic.memi@gmail.com>
2020-12-09 15:02:31 +0100

    Album section minnor changes

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -46,17 +46,19 @@ const columnDefs = [
                             <h1 className="page-title-text">Albums</h1>
                         </div>
                         <div className="albums-content">
-                        <label>Dubble click on cell with asterisk (*) to edit it.</label>
-                            <div className="ag-theme-material" style={ { height: 500, width: "100%" } }>
-                                <AgGridReact 
-                                defaultColDef={{
-                                    floatingFilter: true,
-                                }}
-                                pagination={true}
-                                paginationAutoPageSize={true}
-                                rowData={rowData}
-                                columnDefs={columnDefs}>
-                                </AgGridReact>
+                            <div style={{ width: '100%', height: '100%' }}>
+                                <label>Dubble click on cell with asterisk (*) to edit it.</label>
+                                <div className="ag-theme-material" style={ { height: 500, width: "100%" } }>
+                                    <AgGridReact 
+                                    defaultColDef={{
+                                        floatingFilter: true,
+                                    }}
+                                    pagination={true}
+                                    paginationAutoPageSize={true}
+                                    rowData={rowData}
+                                    columnDefs={columnDefs}>
+                                    </AgGridReact>
+                                </div>
                             </div>
                         </div>
                     </div>
