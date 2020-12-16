00a32f7f492823ed6f04d36d70e39edea58759d8
Nemanja Spasojevic <nemanja.spasojevic.memi@gmail.com>
2020-12-17 00:52:13 +0100

    Add filter Tags

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../assets/style/Tags.css';
 import { Navbar } from './Navbar';
 import { Sidedashbar } from './Sidedashbar';
+import tags from '../data.js'
 
 export const Tags = () => {
+
+    const [array, setArray] = useState([]);
+    const [filteredArray, setFilteredArray] = useState([]);
+
+    useEffect(() => {
+        setFilteredArray(tags);
+        setArray(tags);
+        return (() => {
+            setFilteredArray(null)
+            setArray(null)
+        })
+    }, [])
+
+    const handleInput = (e) => {
+        if (e.target.value === ''){
+            setFilteredArray(array)
+            return
+        }
+        let filteredTags = array.filter(el => el.tag.toLowerCase().includes(e.target.value.toLowerCase()));
+        setFilteredArray(filteredTags)
+    }
+
     return (
         <>
             <Navbar />
@@ -14,8 +37,9 @@ export const Tags = () => {
                             <h1 className="page-title-text">Tags</h1>
                         </div>
                         <div className="tags-content">
-                            <input type="text" className="form-control posts-search col-6 col-sm-3" placeholder="Search for tags..."/>
+                            <input type="text" className="form-control posts-search col-6 col-sm-3" onChange={(e) => handleInput(e)} placeholder="Search for tags..."/>
                         </div>
+                        <div>{filteredArray.map(el => {return <div key={el.id}>{el.tag}</div>})}</div>
                     </div>
                 </div>
         </>
---
+++ src/data.js
@@ -0,0 +1,68 @@
+let tags = [
+    {
+        tag: 'ACA LUKAS',
+        id: '1'
+    },
+    {
+        tag: 'BAD ROMANCE',
+        id: '2'
+    },
+    {
+        tag: 'CARA DELEVIGNE',
+        id: '3'
+    },
+    {
+        tag: 'ČESTITKA',
+        id: '4'
+    },
+    {
+        tag: 'DANICA NIKOLIĆ',
+        id: '5'
+    },
+    {
+        tag: 'ĐEKSON',
+        id: '6'
+    },
+    {
+        tag: 'FACEBOOK',
+        id: '7'
+    },
+    {
+        tag: 'GADE',
+        id: '8'
+    },
+    {
+        tag: 'HIDRATACIJA',
+        id: '9'
+    },
+    {
+        tag: 'INTERNET',
+        id: '10'
+    },
+    {
+        tag: 'JONAS BROTHERS',
+        id: '11'
+    },
+    {
+        tag: 'KATY PERRY',
+        id: '12'
+    },
+    {
+        tag: 'LAPTOP',
+        id: '13'
+    },
+    {
+        tag: 'MUZIKA',
+        id: '14'
+    },
+    {
+        tag: 'NOTRDAM',
+        id: '15'
+    },
+    {
+        tag: 'OPERACIJA',
+        id: '16'
+    }
+]
+
+export default tags
